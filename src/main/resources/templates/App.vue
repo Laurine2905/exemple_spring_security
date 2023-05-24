5b69dcee904b10bf7ee54c420976cde2a9affd25
Laurine2905 <laurine.rat@outlook.fr>
2023-05-24 14:54:39 +0200

    vue.config.js

---
+++ src/main/resources/templates/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Bandeau from "../../../exemple_spring_security/src/main/resources/static/components/Bandeau.vue";
+import Intervenant from "../../../exemple_spring_security/src/main/resources/templates/Intervenant.vue";
+</script>
+
+<template>
+  <body>
+  <div>
+    <Bandeau />
+  </div>
+  </body>
+</template>
+
+<style scoped>
+</style>
