d94e54b513256e9f5232bfc4eb387718f6390ce2
Laurine2905 <laurine.rat@outlook.fr>
2023-05-24 11:34:26 +0200

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pages: {
+        index: {
+            entry: 'template/views/Intervenant.vue', // Chemin vers votre point d'entrée Vue dans le dossier template
+            template: 'template/index.html', // Chemin vers votre fichier HTML dans le dossier template
+            filename: 'index.html', // Nom de sortie du fichier HTML
+        },
+    },
+};
